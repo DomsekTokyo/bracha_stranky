da08be3ce8e52b14bb72ef407d97278ac061e8dd
Domsek_tokyo <153553604+DomsekTokyo@users.noreply.github.com>
2025-11-05 19:55:20 +0100

    responzivita pro navigaci rezervace hotova

--- js/navigaceindex.js
+++ js/navigaceindex.js
@@ -41,7 +41,7 @@ uvod2.addEventListener("click", function(){
 
 document.querySelectorAll('a[href^="#"]').forEach(link => {
     link.addEventListener('click', function(e) {
-        e.preventDefault(); // jen interní scroll
+        e.preventDefault();
         const target = document.querySelector(this.getAttribute('href'));
         if (target) {
             window.scrollTo({ top: target.offsetTop - 70, behavior: 'smooth' });
--- js/rezervace.js
+++ js/rezervace.js
@@ -1,1 +1,13 @@
-
+const button = document.querySelector('.hamburger');
+const menu = document.querySelector(".mobil")
+const cross = document.querySelector(".cross")
+button.addEventListener('click', () => {
+   menu.classList.add("dej")
+    button.style.display = "none";
+   cross.style.display = "block";
+})
+cross.addEventListener('click', () => {
+    menu.classList.remove("dej")
+    button.style.display = "block";
+    cross.style.display = "none";
+})
